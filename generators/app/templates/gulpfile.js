e28f357cdd66950737a54195ff37fffb2b8f7cda
jinam Yu <znu@thenet-global.com>
2017-12-22 17:34:12 +0900

    browsersync update

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -4,6 +4,8 @@ const gulp = require('gulp');
 const path = require('path');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const spawn = require('child_process').spawn;
+
 global.root = path.resolve(__dirname);
 
 const sequence = require('gulp-sequence');
@@ -13,14 +15,36 @@ require('./configuation/task/webpack/script');
 const render = require('./configuation/task/render/script');
 
 let timeout = null;
-gulp.task('watch', () => {
+gulp.task('restart', function () {
+  const keys = Object.keys(browserSync.instance.io.sockets.connected);
+  if (keys.length) {
+    spawn('gulp', ['default', '--browser', false], { stdio: 'inherit' });
+  } else {
+    spawn('gulp', ['default'], { stdio: 'inherit' });
+  }
+
+  process.exit();
+});
+
+const serve = (bool) => {
+  let open = true;
+  if (bool && bool === 'false') {
+    open = false;
+  }
   browserSync.init({
     port: 3400,
+    open: open,
     server: {
       baseDir: './app/prod',
     },
   });
+};
 
+gulp.task('serve', () => {
+  serve();
+});
+
+gulp.task('watch', () => {
   gulp.watch('app/dev/**/*.html', { cwd: './' }, (file) => {
     render(file.path);
   });
@@ -49,6 +73,23 @@ gulp.task('watch', () => {
       if (err) console.log(err);
     });
   });
+
+  gulp.watch(['./gulpfile.js', './webpack.config.js', 'app/dev/nut/**/*.nut', 'app/dev/nut/**/*.html'], ['restart']);
+});
+gulp.task('default', () => {
+
+  let arg = {
+    browser: true,
+  };
+
+  let index = process.argv.indexOf('--browser');
+  if (index !== -1) {
+    arg.browser = process.argv[index + 1];
+  }
+
+  sequence('sass', 'webpack', 'watch')((err) => {
+    if (err) console.log(err);
+    serve(arg.browser);
+  });
 });
 
-gulp.task('default', ['sass', 'webpack', 'watch']);
